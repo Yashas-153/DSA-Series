536fbdc1387bfd4182d9a23d7e1b521145a90358
Yashas_252 <76196714+Yashas-153@users.noreply.github.com>
2023-02-27 23:16:30 +0530

    7th commit

--- Dynamic Programming/LIS.cpp
+++ Dynamic Programming/LIS.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 int LIS(int idx1, int idx2, string s1, string s2, vector<vector<int>> &dp){
     if(idx1 == 0 and idx2 == 0){
-        return s1[idx1] == s2[idx2];
+        return dp[idx1][idx2] = s1[idx1] == s2[idx2];
     }
     if(idx1 < 0 or idx2 < 0){
         return 0;
     }
 
-    if(s1[idx1]== s2[idx2]){
-        return 1 + LIS(idx1-1, idx2- 1, s1,s2,dp);
+    if(dp[idx1][idx2] == -1){
+        return dp[idx1][idx2];
     }
-    return 0 + max(LIS(idx1-1,idx2,s1,s2,dp),LIS(idx1,idx2-1,s1,s2,dp));
+    if(s1[idx1] == s2[idx2]){
+        return dp[idx1][idx2] = 1 + LIS(idx1-1, idx2- 1, s1,s2,dp);
+    }
+    return dp[idx1][idx2] = 0 + max(LIS(idx1-1,idx2,s1,s2,dp),LIS(idx1,idx2-1,s1,s2,dp));
 
 }
 
